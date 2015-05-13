0d76f6c7af40503c446865ab8c3c5097062c6e8b
Emile Taverne <emile_taverne@hotmail.com>
2015-05-13 14:55:42 +0200

    Mise à jour du combat

--- CombatServer/Handlers/CombatHandler.cs
+++ CombatServer/Handlers/CombatHandler.cs
@@ -11,8 +11,18 @@ namespace CombatServer.Handlers
         public Combat Combat { get; set; }
         public bool Active { get; set; }
         public DateTime Time { get; set; }
+
+        //Handler des clients
         private ClientHandler clientHandler1;
         private ClientHandler clientHandler2;
+
+        //Mains des deux joueurs
+        private List<Object> hand1;
+        private List<Object> hand2;
+
+        //Plateaux de jeu
+        private Object[,] table1 = new Object[6,1];
+        private Object[,] table2 = new Object[6,1];
 
         public ClientHandler ClientHandler1
         {
@@ -38,6 +48,8 @@ namespace CombatServer.Handlers
         {
             this.Active = true;
             this.Time = DateTime.Now;
+            this.hand1 = new List<object>();
+            this.hand2 = new List<object>();
         }
 
         private void start()
@@ -48,13 +60,20 @@ namespace CombatServer.Handlers
                 //On initialise le combat
                 Server.AvailableCombats.Remove(this.Combat);
 
-                //On set la vie des joueurs
+                //On set les ressources des joueurs
                 int life1 = 30;
                 int life2 = 30;
+                int energy1 = 0;
+                int energy2 = 0;
 
                 //On détermine le joueur qui commence
                 Random rdm = new Random();
-                rdm.Next(2);
+                int currentPlayer = rdm.Next(2);
+
+                //Mélanger les decks
+
+                //Début tour
+                //Timer turn
             }
         }
     }
